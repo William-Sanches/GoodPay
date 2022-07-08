bd6876a8e8888794668e83ce0c7d121885b20f08
William-Sanches <eng.williamsg@outlook.com>
2022-07-08 10:10:40 -0300

    Adição do sistema de modulos para execução do Login

---
+++ js/index.js
@@ -0,0 +1,22 @@
+import userLogin from "./moduloDados.js";
+const alertaLogin = new bootstrap.Modal("#alertaLogin");
+const entrarUsuario = document.getElementById("btnEntrar");
+
+entrarUsuario.addEventListener("click", _event => {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    let validaLogin = userLogin.find(userLogin => userLogin.userName === login);
+    if (login === validaLogin.userName && senha === validaLogin.senha){
+        window.location.pathname = "/app.html";
+    }else{
+        alertaLogin.show();
+    }
+});
+
+/*     for (i = 0; i < userLogin.length; i++){
+        if(login === userLogin[i].userName && senha === userLogin[i].senha){
+            alert("Paranbéns está Logado");
+        }else{
+            alertaLogin.show();
+        }
+    } */
